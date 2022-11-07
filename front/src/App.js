02fe474be15e1b39bcc308da5fd158ade26042f4
Leandro Barbosa <boyleandro_208@hotmail.com>
2022-11-07 00:12:09 -0300

    obtendo dados da api

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,39 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
-function App() {
 
+function App() {
+  //obj produt
+  const produto = {
+    codigo : 0,
+    nome: '',
+    marca: ''
+  }
+
+  //useState
   const [ btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  //useEffect
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  //pegando dados do formulario
+  const aoDigitar = (e) => {
+      setObjProduto({...objProduto, [e.target.name]:e.target.value});
+  }
 
   return (
     <div>
-      <Formulario botao={btnCadastrar}/>
-      <Tabela/>
+      <p>{JSON.stringify(objProduto)}</p>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar}/>
+      <Tabela vetor={produtos}/>
     </div>
   );
 }
